0201f8b58c99bf1e2d3ccbc4ed39fe2c6f5a2ba4
Larissa Corrêa <larisol.corretora@gmail.com>
2024-08-07 21:26:26 -0300

    feat: página de user profile completamente estilizada, dashboard quase completamente estilizado. falta só uma última parte e implementar acessibilidade

--- HelpDesk/HelpDesk/DashboardView.swift
+++ HelpDesk/HelpDesk/DashboardView.swift
@@ -10,7 +10,6 @@ import Charts
 
 struct DashboardView: View {
     
-    
     @State var selectedTab = "Mensal"
     var tabs = ["Mensal", "Semanal", "Diário"]
     
@@ -32,98 +31,123 @@ struct DashboardView: View {
         SavingsDataPoint(month: "Outros", value: 10)
     ]
     
-    var user = "HD"
+    var user = "HD Team"
     
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 10) {
-            HStack {
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 40, height: 40)
-                    .accessibilityLabel("User Profile Image")
+            VStack(alignment: .leading, spacing: 10) {
 
-                Text("Hello")
-                    .font(.headline)
-                Spacer()
+                HStack {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40)
+                        .accessibilityLabel("User Profile Image")
+                    
+                    Text("Hello")
+                        .font(.headline)
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
+                
+                Text("\(user)")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 50) // Alinhamento do texto com "Hello"
             }
-            .padding(.horizontal, 10)
-
-            Text("Help Desk")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .padding(.leading, 50) // Alinhamento do texto com "Hello"
-        }
-        .padding()
-        
-        VStack(alignment: .leading) {
-            ScrollView {
-                Picker("breakdown", selection: $selectedTab) {
-                    ForEach(tabs, id: \.self) {
-                        Text($0)
-                    }
-                }
-                .pickerStyle(.segmented)
-                
-                // Gráfico de Barras
-                Chart {
-                    ForEach(chartData) { item in
-                        BarMark(
-                            x: .value("Dia", item.month),
-                            y: .value("Valor", item.value)
-                        )
-                    }
-                }
-                .frame(height: 200)
-                .padding()
-                
-                // Gráfico de Pizza
-                Chart {
-                    ForEach(pieChartData) { item in
-                        SectorMark(
-                            angle: .value("Quantidade", item.value),
-                            innerRadius: .ratio(0.5),
-                            outerRadius: .ratio(1.0)
-                        )
-                        .foregroundStyle(by: .value("Categoria", item.month))
-                        .annotation(position: .overlay) {
-                            Text("\(Int(item.value))")
-                                .font(.caption)
-                                .foregroundColor(.white)
+            .padding()
+            
+            VStack(alignment: .leading) {
+                ScrollView {
+                    Picker("breakdown", selection: $selectedTab) {
+                        ForEach(tabs, id: \.self) {
+                            Text($0)
                         }
                     }
+                    .pickerStyle(.segmented)
+                    
+                    // Gráfico de Barras
+                    Chart {
+                        ForEach(chartData) { item in
+                            BarMark(
+                                x: .value("Dia", item.month),
+                                y: .value("Valor", item.value)
+                            )
+                        }
+                    }
+                    .frame(height: 200)
+                    .padding()
+                    
+                    // Gráfico de Pizza
+                    Chart {
+                        ForEach(pieChartData) { item in
+                            SectorMark(
+                                angle: .value("Quantidade", item.value),
+                                innerRadius: .ratio(0.5),
+                                outerRadius: .ratio(1.0)
+                            )
+                            .foregroundStyle(by: .value("Categoria", item.month))
+                            .annotation(position: .overlay) {
+                                Text("\(Int(item.value))")
+                                    .font(.caption)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                    .frame(height: 200)
+                    .padding()
+                    
+                    VStack(spacing: 20) { // Add some spacing between boxes
+                        HStack(spacing: 10) { // Add spacing between elements in each box
+                            Text("30")
+                            Text("Qtd de chamados abertos")
+                            
+                            Text("60")
+                            Text("Qtd de chamados fechados")
+                            
+                        }
+                        .padding()
+
+                        
+                        HStack(spacing: 10) {
+                            Text("60 min")
+                            Text("Tempo médio de respostas")
+                            
+                            Text("1 mil")
+                            Text("Qtd de chamados totais")
+                    
+                        }             .padding()
+
+                        
+                        
+                    }
+                    .padding()
+                    .background(Color.blueTertiary)
+                    .cornerRadius(12)
+                    .frame(maxWidth: .infinity)
                 }
-                .frame(height: 200)
                 .padding()
-                
+
                 VStack {
-                    VStack {
-                        HStack {
-                            HStack {
-                                Text("30").frame(width: 60, height: 60)
-                                Text("Qtd de chamados abertos").frame(width: 100, height: 100)
-                            }
-                            HStack {
-                                Text("60").frame(width: 50, height: 50)
-                                Text("Qtd de chamados fechados").frame(width: 100, height: 100)
-                            }.frame(width: 230)
-                        }
-                        HStack {
-                            HStack {
-                                Text("60 min").frame(width: 50, height: 50)
-                                Text("Tempo médio de respostas").frame(width: 100, height: 100)
-                            }
-                            HStack {
-                                Text("1 mil").frame(width: 50, height: 50)
-                                Text("Qtd de chamados totais").frame(width: 100, height: 100)
-                            }.frame(width: 230)
-                        }
+                    
+                    HStack {
+                        Image(.useer)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Text("\(user)")
+                            .bold()
+                        Image(systemName: )
                     }
-                }.background(Color.blueTertiary)
+                    Text("Lorem ipsum dolor sit amet. At odit magnam et mollitia commodi qui exercitationem quidem. Et voluptatem harum in quia ipsa qui expedita autem aut maiores quae.")
+                        .fontWeight(.light)
+                        .font(.system(size: 14))
+                }.multilineTextAlignment(.leading)
             }
-        }
-    }
+            
+        } .padding(.top, 50)
+        .background(Color.bluePrimary) // Set the entire background color
+            .edgesIgnoringSafeArea(.all) // Extend the background to the edges of the screen
+}
     
     struct SavingsDataPoint: Identifiable {
         let month: String
