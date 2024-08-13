db556b5fd27bcca57426a9ab1c6a2c23c8680138
Larissa Corrêa <larisol.corretora@gmail.com>
2024-08-13 18:27:30 -0300

    funcionando

--- HelpDesk/HelpDesk/DashboardView.swift
+++ HelpDesk/HelpDesk/DashboardView.swift
@@ -2,11 +2,14 @@ import SwiftUI
 import Charts
 
 struct DashboardView: View {
-    
-    @State var selectedTab = "Mensal"
-    var tabs = ["Mensal", "Semanal", "Diário"]
+
+    @State var selectedTab = "Diário"
+    var tabs = ["Diário", "Semanal", "Mensal"]
     
     var month = ["May", "Jun", "Jul", "Aug", "Setem"]
+    var weeks = [""]
+    var days = ["Seg", "Ter", "Quar", "Quin", "Sex"]
+    var indicador = ["Chamados Abertos", "Chamados Fechados", "Outros"]
     
     // Dados do gráfico de barras
     var chartData = [
@@ -19,9 +22,9 @@ struct DashboardView: View {
     
     // Dados do gráfico de pizza
     var pieChartData = [
-        SavingsDataPoint(month: "Chamados Abertos", value: 30),
-        SavingsDataPoint(month: "Chamados Fechados", value: 60),
-        SavingsDataPoint(month: "Outros", value: 10)
+        PieChartDataPoint(indicador: "Chamados Abertos", value: 30),
+        PieChartDataPoint(indicador: "Chamados Fechados", value: 60),
+        PieChartDataPoint(indicador: "Outros", value: 10)
     ]
     
     // Dados do gráfico de linha para as avaliações
@@ -34,8 +37,8 @@ struct DashboardView: View {
         SavingsDataPoint(month: "Jun", value: 5.0)
     ]
     
-    var user = "HD Team"
-    var grade = 5
+    var user: String = "HD Team"
+    var grade: Int = 5
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
@@ -44,7 +47,6 @@ struct DashboardView: View {
                 HStack {
                     Image(systemName: "person.circle")
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
                         .frame(width: 40, height: 40)
                         .accessibilityLabel("User Profile Image")
                     
@@ -61,7 +63,7 @@ struct DashboardView: View {
                 Text("\(user)")
                     .font(.subheadline)
                     .fontWeight(.semibold)
-                    .padding(.leading, 50) // Alinhamento do texto com "Hello"
+                    .padding(.leading, 50)
             }
             .padding()
             
@@ -78,7 +80,7 @@ struct DashboardView: View {
                     Chart {
                         ForEach(chartData) { item in
                             BarMark(
-                                x: .value("Dia", item.month),
+                                x: .value("Dia", item.month ?? ""), // Descompactando o valor opcional
                                 y: .value("Valor", item.value)
                             )
                         }
@@ -94,7 +96,7 @@ struct DashboardView: View {
                                 innerRadius: .ratio(0.5),
                                 outerRadius: .ratio(1.0)
                             )
-                            .foregroundStyle(by: .value("Categoria", item.month))
+                            .foregroundStyle(by: .value("Categoria", item.indicador))
                             .annotation(position: .overlay) {
                                 Text("\(Int(item.value))")
                                     .font(.caption)
@@ -105,27 +107,24 @@ struct DashboardView: View {
                     .frame(height: 200)
                     .padding()
                     
-                    VStack(spacing: 20) { // Add some spacing between boxes
-                        HStack(spacing: 10) { // Add spacing between elements in each box
+                    VStack(spacing: 20) {
+                        HStack {
                             Text("30")
                             Text("Qtd de chamados abertos")
                             
                             Text("60")
                             Text("Qtd de chamados fechados")
-                            
                         }
                         .padding()
                         
-                        HStack(spacing: 10) {
+                        HStack {
                             Text("60 min")
                             Text("Tempo médio de respostas")
                             
                             Text("1 mil")
                             Text("Qtd de chamados totais")
-                    
                         }
                         .padding()
-                        
                     }
                     .padding()
                     .background(Color.blueTertiary)
@@ -133,23 +132,25 @@ struct DashboardView: View {
                     .frame(maxWidth: .infinity)
                     .padding()
 
-
                     VStack {
                         Text("Como somos vistos por nossos usuários?")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).font(.title2).multilineTextAlignment(.trailing)
+                            .fontWeight(.bold)
+                            .font(.title2)
+                            .multilineTextAlignment(.trailing)
                         
                         HStack {
                             VStack {
-                                Image(.useer)
+                                Image(systemName: "person.circle")
                                     .resizable()
                                     .frame(width: 30, height: 30)
                                 Text("\(user)")
                                     .bold()
                             }
-            
+
                             JustifiedTextView(
                                 text: "Lorem ipsum dolor sit amet. At odit magnam et mollitia commodi qui exercitationem quidem.",
-                                lineWidth: 130)
+                                lineWidth: 130
+                            )
                             .fontWeight(.light)
                             .font(.system(size: 14))
                             
@@ -157,12 +158,11 @@ struct DashboardView: View {
                                 Text("1 mil avaliações")
                                     .textCase(.uppercase)
                                     .font(.system(size: 12))
-                                 
-                                 
-                                 Text("\(grade)")
-                                     .bold()
-                                     .padding(.horizontal, 50)
-                                     .font(.system(size: 28))
+                                
+                                Text("\(grade)")
+                                    .bold()
+                                    .padding(.horizontal, 50)
+                                    .font(.system(size: 28))
                                 
                                 HStack {
                                     Image(.star)
@@ -181,47 +181,44 @@ struct DashboardView: View {
                                         .resizable()
                                         .frame(width: 23, height: 23)
                                 }
-                                
-                            
-                                      
                             }
-                            
-                
                         }
+
                         // Gráfico de Linha para Avaliações
                         VStack(alignment: .leading) {
                             Text("Avaliações ao longo do tempo")
                                 .font(.headline)
                                 .padding(10)
-                                
                             
                             Chart {
                                 ForEach(lineChartData) { item in
                                     LineMark(
-                                        x: .value("Mês", item.month),
+                                        x: .value("Mês", item.month ?? ""), // Descompactando o valor opcional
                                         y: .value("Nota", item.value)
                                     )
                                 }
                             }
                             .frame(height: 200)
                             .padding()
-                             // Define o valor máximo que o gráfico pode ir
                         }
-                        
-                        
-                    }.multilineTextAlignment(.leading)
-                    
+                    }
+                    .multilineTextAlignment(.leading)
                 }
-                
             }
         }
         .padding(.top, 50)
-        .background(Color.bluePrimary)
+        .background(Color.blue.opacity(0.05))
         .edgesIgnoringSafeArea(.all)
     }
     
     struct SavingsDataPoint: Identifiable {
-        let month: String
+        let month: String?
+        let value: Double
+        var id = UUID()
+    }
+    
+    struct PieChartDataPoint: Identifiable {
+        let indicador: String
         let value: Double
         var id = UUID()
     }
