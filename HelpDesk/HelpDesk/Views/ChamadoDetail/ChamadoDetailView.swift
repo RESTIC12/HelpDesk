5c9c1054083d8d59d1b1f77a6dc80005d3fad8f8
victorbrigido <victorbrigido@icloud.com>
2024-08-14 20:43:49 -0300

    feat: tela de usuario criado e logoff implementado

--- HelpDesk/HelpDesk/Views/ChamadoDetail/ChamadoDetailView.swift
+++ HelpDesk/HelpDesk/Views/ChamadoDetail/ChamadoDetailView.swift
@@ -16,6 +16,7 @@ struct ChamadoDetailView: View {
     @StateObject var viewModel: ChamadoDetailViewModel
     
     var body: some View {
+        ZStack {
             VStack(alignment: .leading, spacing: 10) {
                 Text(tituloChamado)
                     .font(.custom("Poppins-Medium", size: 26))
@@ -38,7 +39,7 @@ struct ChamadoDetailView: View {
                 }
                 Text("Descrição:")
                     .font(.custom("Poppins-medium", size: 16))
-                    
+                
                 HStack {
                     Text(descricaoChamado)
                         .font(.custom("Poppins-light", size: 16))
@@ -46,57 +47,58 @@ struct ChamadoDetailView: View {
                     
                     Spacer()
                 }
-                .frame(width: .infinity)
-                    
+                
                 Spacer()
             }
-          
+            
             .padding()
             
-        VStack {
-            Spacer()
-            VStack(spacing: 20) {
-                let permissao = SessionManager.shared.currentUser?.permissao
-                if (permissao == 1 || permissao == 2) && viewModel.help.details.solucionado == false {
-                    Button(action: {
-                        viewModel.updateHelp { success in
-                            if !success {
-                                showAlert = true
+            
+            VStack {
+                Spacer()
+                VStack(spacing: 20) {
+                    let permissao = SessionManager.shared.currentUser?.permissao
+                    if (permissao == 1 || permissao == 2) && viewModel.help.details.solucionado == false {
+                        Button(action: {
+                            viewModel.updateHelp { success in
+                                if !success {
+                                    showAlert = true
+                                }
                             }
+                        }) {
+                            Text("Encerrar Chamado")
+                                .font(.custom("Poppins-Medium", size: 16))
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.callMeDesk)
+                                .cornerRadius(10)
                         }
-                    }) {
-                        Text("Encerrar Chamado")
-                            .font(.custom("Poppins-Medium", size: 16))
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.callMeDesk)
-                            .cornerRadius(10)
+                        .accessibilityLabel(Text("Encerrar chamado"))
                     }
-                    .accessibilityLabel(Text("Encerrar chamado"))
+                    
+                    //                    Button(action: {
+                    //                        print("chamado em andamento button clicked")
+                    //                    }) {
+                    //                        Text("Chamado em andamento")
+                    //                            .foregroundColor(.black)
+                    //                            .padding()
+                    //                            .frame(maxWidth: .infinity)
+                    //                            .background(Color.yellow)
+                    //                            .cornerRadius(10)
+                    //                    }
+                    //                    .accessibilityLabel(Text("chamado em andamento"))
                 }
-                
-                //                    Button(action: {
-                //                        print("chamado em andamento button clicked")
-                //                    }) {
-                //                        Text("Chamado em andamento")
-                //                            .foregroundColor(.black)
-                //                            .padding()
-                //                            .frame(maxWidth: .infinity)
-                //                            .background(Color.yellow)
-                //                            .cornerRadius(10)
-                //                    }
-                //                    .accessibilityLabel(Text("chamado em andamento"))
+                .padding([.leading, .trailing, .bottom])
             }
-            .padding([.leading, .trailing, .bottom])
-        }
-        .frame(maxWidth: .infinity)
-        .alert("Ops!", isPresented: $showAlert) {
-            Button("OK", role: .cancel) {
-                showAlert = false
+            .frame(maxWidth: .infinity)
+            .alert("Ops!", isPresented: $showAlert) {
+                Button("OK", role: .cancel) {
+                    showAlert = false
+                }
+            } message: {
+                Text("Ocorreu um erro inesperado ao tentar encerrar o chamado. Por favor, tente novamente mais tarde.")
             }
-        } message: {
-            Text("Ocorreu um erro inesperado ao tentar encerrar o chamado. Por favor, tente novamente mais tarde.")
         }
         .background(Color.backGround)
     }
