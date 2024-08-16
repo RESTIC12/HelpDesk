9fa55e8e1485a03b13c5b75bb7a7780c0c8f9bcc
victorbrigido <victorbrigido@icloud.com>
2024-08-16 20:12:34 -0300

    chamados implementados no firebase

--- HelpDesk/HelpDesk/API/HelpService.swift
+++ HelpDesk/HelpDesk/API/HelpService.swift
@@ -9,6 +9,7 @@ import Foundation
 import FirebaseFirestore
 import FirebaseAuth
 import UserNotifications
+import SwiftJWT
 
 public enum HelpDeskResultError: Swift.Error {
      case withoutConnectivity
@@ -77,7 +78,6 @@ public final class HelpServiceImp: HelpService {
                 } else {
                     self.notifyUsersAboutNewChamado(for: call)
                     completion(true)
-                    self.testNotification()
                 }
             })
         } catch let error {
@@ -88,8 +88,8 @@ public final class HelpServiceImp: HelpService {
 
     private func notifyUsersAboutNewChamado(for call: HelpDesk) {
         // Aqui você vai buscar os usuários que devem ser notificados.
-        let department = call.details.departamento
-        let query = db.collection("usuarios").whereField("departamento", isEqualTo: department)
+        let permissao = call.details.departamento == "TI" ? 1 : 2
+        let query = db.collection("usuarios").whereField("permissao", isEqualTo: permissao)
 
         query.getDocuments { snapshot, error in
             if let error = error {
@@ -104,7 +104,7 @@ public final class HelpServiceImp: HelpService {
 
             for document in documents {
                 if let fcmToken = document.data()["fcmToken"] as? String {
-                    self.sendPushNotification(to: fcmToken, title: "Novo Chamado", body: "Você recebeu um novo chamado no departamento \(department).")
+                    self.sendPushNotification(to: fcmToken, title: "Novo Chamado", body: "Você recebeu um novo chamado no departamento \(permissao).")
                 }
             }
         }
@@ -130,27 +130,27 @@ public final class HelpServiceImp: HelpService {
     }
     
     func sendPushNotification(to token: String, title: String, body: String) {
-        let urlString = "https://fcm.googleapis.com/fcm/send"
-        let url = URL(string: urlString)!
+        guard let serverFcmToken = generateAccessToken() else { return }
+        guard let url = URL(string: "https://fcm.googleapis.com/v1/projects/helpDesk-Group1/messages:send") else {
+            print("URL inválida.")
+            return
+        }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("key=540cd806c9f74e823f7cd18db152469ad5a986cf", forHTTPHeaderField: "Authorization")  // Substitua com sua chave do servidor FCM
+        request.setValue("Bearer \(serverFcmToken)", forHTTPHeaderField: "Authorization")  // Substitua com sua chave do servidor FCM
         
-        let notification = [
-                "to": token,
+        let payload: [String: Any] = [
+            "message": [
+                "token": token,
                 "notification": [
-                    "title": title,
-                    "body": body,
-                    "sound": "default"
-                ],
-                "data": [
-                    "title": title,
-                    "body": body
+                    "title": "Novo Chamado Criado",
+                    "body": "Um novo chamado foi criado por um usuário com permissão 0."
                 ]
-            ] as [String : Any]
+            ]
+        ]
         
-        let jsonData = try? JSONSerialization.data(withJSONObject: notification, options: [])
+        let jsonData = try? JSONSerialization.data(withJSONObject: payload, options: [])
         request.httpBody = jsonData
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
@@ -158,8 +158,14 @@ public final class HelpServiceImp: HelpService {
                 print("Erro ao enviar notificação: \(error.localizedDescription)")
                 return
             }
-            print("Notificação enviada com sucesso")
-            print(notification)
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    print("Notificação enviada com sucesso")
+                    print(payload)
+                } else {
+                    print("Erro ao enviar notificação, statusCode: \(httpResponse.statusCode)")
+                }
+            }
         }
         task.resume()
     }
@@ -169,7 +175,101 @@ public final class HelpServiceImp: HelpService {
         sendPushNotification(to: testToken, title: "Test Notification", body: "This is a test notification.")
         print("funcao notificacao chamada")
     }
+
+    struct MyClaims: Claims {
+        let iss: String
+        let scope: String
+        let aud: String
+        let iat: Date
+        let exp: Date
+    }
+
+    func generateAccessToken() -> String? {
+        // Caminho para o arquivo JSON da conta de serviço
+        guard let serviceAccountPath = Bundle.main.path(forResource: "helpdesk-group1-d95eed309262", ofType: "json"),
+              let data = try? Data(contentsOf: URL(fileURLWithPath: serviceAccountPath)),
+              let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+              let clientEmail = json["client_email"] as? String,
+              let privateKey = json["private_key"] as? String else {
+            print("Erro ao carregar as credenciais da conta de serviço.")
+            return nil
+        }
+        
+        // Preparar os dados para o JWT
+        let iat = Date()
+        let exp = iat.addingTimeInterval(3600) // 1 hora de validade
+        let claims = MyClaims(
+            iss: clientEmail,
+            scope: "https://www.googleapis.com/auth/firebase.messaging",
+            aud: "https://oauth2.googleapis.com/token",
+            iat: iat,
+            exp: exp
+        )
+        
+        var jwt = JWT(claims: claims)
+        let jwtSigner = JWTSigner.rs256(privateKey: privateKey.data(using: .utf8)!)
+
+        do {
+            let signedJWT = try jwt.sign(using: jwtSigner)
+            return signedJWT
+        } catch {
+            print("Erro ao assinar o JWT: \(error)")
+            return nil
+        }
+    }
     
+
+    func sendPushNotification() {
+        guard let accessToken = generateAccessToken() else {
+            print("Erro ao gerar o token de acesso.")
+            return
+        }
+        
+        guard let url = URL(string: "https://fcm.googleapis.com/v1/projects/YOUR_PROJECT_ID/messages:send") else {
+            print("URL inválida.")
+            return
+        }
+        
+        let payload: [String: Any] = [
+            "message": [
+                "notification": [
+                    "title": "Novo Chamado Criado",
+                    "body": "Um novo chamado foi criado por um usuário com permissão 0."
+                ],
+                "topic": "users_with_permission_1_or_2"
+            ]
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+        } catch {
+            print("Erro ao converter JSON: \(error.localizedDescription)")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Erro ao enviar notificação: \(error.localizedDescription)")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    print("Notificação enviada com sucesso.")
+                } else {
+                    print("Erro ao enviar notificação, statusCode: \(httpResponse.statusCode)")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+
 //    func scheduleRepeatingNotification() {
 //            let content = UNMutableNotificationContent()
 //            content.title = "Teste de Notificação"
@@ -265,7 +365,7 @@ public final class HelpServiceImp: HelpService {
 //         request.setValue("\(String(describing: jsonData.count))", forHTTPHeaderField: "Content-Length")
 //         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 //         request.httpBody = jsonData
-//         
+//
 //         URLSession.shared.dataTask(with: request) { data, response, error in
 //             guard let data = data,
 //                   let response = response as? HTTPURLResponse,
