f42a06ae0b439b45d0a288addbca097392692a73
victorbrigido <victorbrigido@icloud.com>
2024-08-23 19:06:25 -0300

    fix: bug ao iniciar o app com usuario logado

--- HelpDesk/HelpDesk/Views/HomeView/HomeView.swift
+++ HelpDesk/HelpDesk/Views/HomeView/HomeView.swift
@@ -15,97 +15,18 @@ struct HomeView: View {
     @State var showNewChamadoView = false
     @State var searchText: String = ""
     @State var openHelpOrNotText: String = "Em aberto"
-    @StateObject var viewModel = HomeViewModel(
-            service: HelpServiceImp()
-        )
+    @StateObject var viewModel = HomeViewModel(service: HelpServiceImp())
     @State private var isShowingLoginView = false
-    
+    @ObservedObject var sessionManager = SessionManager.shared 
 
     var body: some View {
-        
         NavigationView {
             VStack {
-                HeaderView(searchText: $searchText) {
-                    viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
-                        showAlert = true
-                    })
-                } dismissUserView: {
-                    isShowingLoginView = true
+                if sessionManager.userDataLoaded {
+                    content
+                } else {
+                    ProgressView("Carregando...")
                 }
-                
-                
-                Picker("", selection: $openHelpOrNotText) {
-                    ForEach(statusHelp, id: \.self) {
-                        Text($0)
-                    }
-                }
-                .onReceive([openHelpOrNotText].publisher.first(), perform: { _ in
-                    viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
-                        showAlert = true
-                    })
-                })
-                .pickerStyle(.segmented)
-                .padding(.horizontal)
-                .padding(.bottom, 15)
-                .fullScreenCover(isPresented: $isShowingLoginView, content: {
-                    LoginView()
-                })
-                
-                let permissao = SessionManager.shared.currentUser?.permissao
-                HStack {
-                    Spacer()
-                    if permissao == 0 {
-                        Button {
-                            showNewChamadoView.toggle()
-                        } label: {
-                            Label("Novo ticket", systemImage: "plus")
-                                .padding()
-                                .foregroundStyle(.white)
-                        }
-                        .frame(height: 35)
-                        .background(Color(hex: "#44C4FC"))
-                        .cornerRadius(12)
-                    }
-                }
-                .padding(.horizontal, 20)
-                ScrollView(showsIndicators: false) {
-                    ForEach(viewModel.helps, id: \.self) { call in
-                        NavigationLink(destination: ChamadoDetailView(
-                            tituloChamado: call.details.titulo,
-                            descricaoChamado: call.details.texto,
-                            solicitante: call.details.solicitante,
-                            prioridade: call.details.prioridade,
-                            viewModel: ChamadoDetailViewModel(help: call)
-                        )) {
-                            CardView(
-                                tituloChamado: call.details.titulo,
-                                descricaoChamado: call.details.texto,
-                                prioridadeChamado: call.details.prioridade,
-                                departamentoChamado: "Departamento \(call.details.departamento)", 
-                                solicitanteChamado: call.details.solicitante
-                            )
-                        }
-                        .foregroundColor(.black)
-                    }
-                }
-                .onAppear() {
-                    viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
-                        showAlert = true
-                    })
-                }
-                .sheet(isPresented: $showNewChamadoView) {
-                    NewChamadoView(
-                        isPresented: $showNewChamadoView,
-                        fetchHelps: {
-                            viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
-                                showAlert = true
-                            })
-                        }
-                    )
-                }
-                .padding()
-                Spacer()
-                
             }
             .background(Color.backGround)
         }
@@ -119,9 +40,91 @@ struct HomeView: View {
         } message: {
             Text("Ocorreu um erro inesperado ao tentar obter os chamados.")
         }
-
     }
-    
+
+    var content: some View {
+        VStack {
+            HeaderView(searchText: $searchText) {
+                viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
+                    showAlert = true
+                })
+            } dismissUserView: {
+                isShowingLoginView = true
+            }
+
+            Picker("", selection: $openHelpOrNotText) {
+                ForEach(statusHelp, id: \.self) {
+                    Text($0)
+                }
+            }
+            .onReceive([openHelpOrNotText].publisher.first(), perform: { _ in
+                viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
+                    showAlert = true
+                })
+            })
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            .padding(.bottom, 15)
+            .fullScreenCover(isPresented: $isShowingLoginView, content: {
+                LoginView()
+            })
+
+            let permissao = sessionManager.currentUser?.permissao
+            HStack {
+                Spacer()
+                if permissao == 0 {
+                    Button {
+                        showNewChamadoView.toggle()
+                    } label: {
+                        Label("Novo ticket", systemImage: "plus")
+                            .padding()
+                            .foregroundStyle(.white)
+                    }
+                    .frame(height: 35)
+                    .background(Color(hex: "#44C4FC"))
+                    .cornerRadius(12)
+                }
+            }
+            .padding(.horizontal, 20)
+            ScrollView(showsIndicators: false) {
+                ForEach(viewModel.helps, id: \.self) { call in
+                    NavigationLink(destination: ChamadoDetailView(
+                        tituloChamado: call.details.titulo,
+                        descricaoChamado: call.details.texto,
+                        solicitante: call.details.solicitante,
+                        prioridade: call.details.prioridade,
+                        viewModel: ChamadoDetailViewModel(help: call)
+                    )) {
+                        CardView(
+                            tituloChamado: call.details.titulo,
+                            descricaoChamado: call.details.texto,
+                            prioridadeChamado: call.details.prioridade,
+                            departamentoChamado: "Departamento \(call.details.departamento)",
+                            solicitanteChamado: call.details.solicitante
+                        )
+                    }
+                    .foregroundColor(.black)
+                }
+            }
+            .onAppear() {
+                viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
+                    showAlert = true
+                })
+            }
+            .sheet(isPresented: $showNewChamadoView) {
+                NewChamadoView(
+                    isPresented: $showNewChamadoView,
+                    fetchHelps: {
+                        viewModel.fetchChamados(filter: searchText, solucionado: openHelpOrNotText == "Concluídos", completion: {
+                            showAlert = true
+                        })
+                    }
+                )
+            }
+            .padding()
+            Spacer()
+        }
+    }
 }
 
 #Preview {
